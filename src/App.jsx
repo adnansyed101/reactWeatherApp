d97fea76bf1ad24fad77236cc4aa1ea93dd0ebf3
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-09-14 17:56:16 +0600

    add logic for fetching data
    in use effect hook first the geo location of the browser is being found. after that another fetch gets the weather data. Also error handling is being done here

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,80 @@
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+
 const App = () => {
-  return <h1 className="text-2xl">Hello from App</h1>;
+  const [isFarenheit, setIsFarenheit] = useState(true);
+  const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState("London");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const API_KEY = "9ee2353a7a9004a8d0d3701c47840223";
+
+  useEffect(() => {
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`;
+
+    function getWeatherData(name, latitude, longitude) {
+      const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+      fetch(WEATHER_API_URL, { mode: "cors" })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.cod >= 400) {
+            setError("There was some error. Please try again later");
+          } else {
+            setWeatherData(data);
+          }
+        })
+        .catch((err) => {
+          setError(err);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+
+    function fetchWeatherData() {
+      fetch(GEOCODING_API_URL, { mode: "cors" })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.cod >= 400) {
+            setError(data.message + " Please fix location");
+          } else {
+            const { name, lat, lon } = data[0];
+            getWeatherData(name, lat, lon);
+          }
+        })
+        .catch((err) => {
+          setError(err.message);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+
+    fetchWeatherData();
+  }, [location]);
+
+  const toggleIsFarenheit = () => {
+    setIsFarenheit((prev) => !prev);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLocation(e.target.locationInput.value.trim());
+  };
+
+  return (
+    <div className="App">
+      <Header
+        isFarenheit={isFarenheit}
+        toggleIsFarenheit={toggleIsFarenheit}
+        handleSubmit={onSubmit}
+      />
+      {loading && <p>Loading....</p>}
+      {error && <p>{error}</p>}
+    </div>
+  );
 };
 
 export default App;
